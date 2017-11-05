ec1bfdf0f9c1c1d1a89ed126c4b4a5cb512ef4d9
Garrett Miller <garrettstillwater@gmail.com>
2017-11-05 17:27:45 -0600

    Added MCP9808

--- Basic Sensor Examples/SensorRead/SensorRead.ino
+++ Basic Sensor Examples/SensorRead/SensorRead.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include "Adafruit_MCP9808.h"
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("MCP9808 demo");
+
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find MCP9808!");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  float c = tempsensor.readTempC();
+  float f = c * 9.0 / 5.0 + 32;
+  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); 
+  Serial.print(f); Serial.println("*F");
+
+  delay(1000);
 
 }
