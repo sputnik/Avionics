10fbc5ab96f9d885f66940a09295903d696723a1
djmallek <djmallek1@gmail.com>
2017-11-05 17:47:44 -0600

    Add ADXL Print, clean print of MPC and MPL

--- Basic Sensor Examples/SensorRead/SensorRead.ino
+++ Basic Sensor Examples/SensorRead/SensorRead.ino
@@ -1,9 +1,4 @@
 
-<<<<<<< HEAD
-void setup() {
-  
-
-=======
 #include "Adafruit_MCP9808.h"
 #include <Adafruit_MPL115A2.h>
 
@@ -13,7 +8,6 @@ Adafruit_MPL115A2 mpl115a2;
 void setup() {
   Serial.begin(9600);
   Serial.println("MCP9808 demo");
->>>>>>> ec1bfdf0f9c1c1d1a89ed126c4b4a5cb512ef4d9
 
   if (!tempsensor.begin()) {
     Serial.println("Couldn't find MCP9808!");
@@ -23,27 +17,31 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< HEAD
-    int ADXL377_X_axis = analogRead(A0);
-    int ADXL377_Y_axis = analogRead(A1);
-    int ADXL377_Z_axis = analogRead(A3);
-=======
-  // put your main code here, to run repeatedly:
+   //ADXL377
+   int ADXL377_X_axis = analogRead(A0);
+   int ADXL377_Y_axis = analogRead(A1);
+   int ADXL377_Z_axis = analogRead(A2);
+   Serial.print("ADXL377: \t");
+   Serial.print("X Axis: "); Serial.print(ADXL377_X_axis);
+   Serial.print("\t Y Axis: "); Serial.print(ADXL377_Y_axis);
+   Serial.print("\t Z Axis: "); Serial.println(ADXL377_Z_axis);
 
+  //MCP9808
   float c = tempsensor.readTempC();
   float f = c * 9.0 / 5.0 + 32;
+  Serial.print("MCP9808 \t");
   Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); 
   Serial.print(f); Serial.println("*F");
 
+  //MPL115A2
   float pressureKPA = 0, temperatureC = 0;  
   mpl115a2.getPT(&pressureKPA,&temperatureC);
+  Serial.print("MPL115A2: \t");
   Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.print(" kPa  ");
-  Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C both measured together");
+  Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
 
 
 
 
   delay(1000);
-
->>>>>>> ec1bfdf0f9c1c1d1a89ed126c4b4a5cb512ef4d9
 }
