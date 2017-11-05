10698bfb153d6a17b1b34dcf22823b4b39a39c79
djmallek <djmallek1@gmail.com>
2017-11-05 17:32:15 -0600

    Add ADXL377

--- Basic Sensor Examples/SensorRead/SensorRead.ino
+++ Basic Sensor Examples/SensorRead/SensorRead.ino
@@ -1,9 +1,12 @@
+
 void setup() {
-  // put your setup code here, to run once:
+  
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    int ADXL377_X_axis = analogRead(A0);
+    int ADXL377_Y_axis = analogRead(A1);
+    int ADXL377_Z_axis = analogRead(A3);
 }
