506b38c26ee3bfe8c95fef2a11888df13922fdb6
JakeSammons <jsammons@iastate.edu>
2018-01-17 19:37:22 -0600

    Correct values as of current rocket
    added correct values for the rocket weight, burn rate, thrust curve etc

--- flight.c
+++ flight.c
@@ -4,16 +4,16 @@
 #define G 9.81 //meters per second squared
 #define SEA_DENSITY 1.225 //kg per cubic meter
 #define TIME_DELTA 0.1 //seconds
-#define CD_BRAKE 1.28
-#define CD_DRAG 0.75
-#define AREA_ROCKET .0248286665 //meters squared
-#define AREA_BRAKE .01032256 //meters squared
+#define CD_BRAKE 1
+#define CD_DRAG 0.4
+#define AREA_ROCKET .0182414692 //meters squared
+#define AREA_BRAKE .0174 //meters squared
 #define START_ALT 1219.2 //meters
-#define GOAL_HEIGHT 7000 //meters
-#define WEIGHT_I 380.75175 //newtons
-#define WEIGHT_F 198.18765 //newtons
-#define START_THRUST 8500 //newtons
-#define FUEL_BURN_RATE 34.446 //newtons per second
+#define GOAL_HEIGHT 3048 //meters
+#define WEIGHT_I 373.651 //newtons
+#define WEIGHT_F 306.927 //newtons
+#define START_THRUST 2500 //newtons
+#define FUEL_BURN_RATE 15.44504651 //newtons per second
 
 
 double density_from_altitude(double altitude) //density equation
@@ -26,7 +26,7 @@ double drag_from_density_and_velocity(double dense, double vel) //drag equation 
 }
 double brake_from_density_and_velocity(double dense, double vel) //drag equation for airbrakes
 {
-	return .5 * dense * vel * vel * CD_BRAKE * AREA_ROCKET;
+	return .5 * dense * vel * vel * CD_BRAKE * AREA_BRAKE;
 }
 
 int main(void)
@@ -47,9 +47,12 @@ int main(void)
 
 	thrust = START_THRUST; //newtons
 	weight = WEIGHT_I; //newtons
-	for (time = 0; time < 5.3; time += TIME_DELTA) {
-		if (time >= 2.5) {
-			thrust -= (time < 4.9 ? 400 : 18750) * TIME_DELTA;
+	for (time = 0; time < 4.4; time += TIME_DELTA) {
+		if (time <= 2.0) {
+			thrust += 250 * TIME_DELTA;
+		}
+		else if (time > 2.0) {
+			thrust -= (time < 4.0 ? 250 : 6250) * TIME_DELTA;
 		} //thrust equation
 
 		alt += velocity * TIME_DELTA; //meters
@@ -61,11 +64,24 @@ int main(void)
 		mass = weight / G; //kg
 		velocity += force / mass * TIME_DELTA; //meters per second
 
-		printf("height = %f\nvelocity = %f\ntime = %f\n\n", height, velocity, time);
+		printf("height = %f\nvelocity = %f\nthrust = %f\ntime = %f\n\n", height, velocity, thrust, time);
 	}
 
 	weight = WEIGHT_F; //newtons
 	mass = weight / G; //kg
+	for (; time < 7.3; time += TIME_DELTA) {
+		tend = (GOAL_HEIGHT - height) * 2 / velocity; //seconds
+		fneed = velocity * mass / tend; //newtons
+		alt += velocity * TIME_DELTA; //meters
+		height = alt - START_ALT; //meters
+		density = density_from_altitude(alt); //kg per cubic meter
+		drag = drag_from_density_and_velocity(density, velocity); //newtons
+		brake = brake_from_density_and_velocity(density, velocity); //newtons
+		force = -drag - weight; //newtons
+		printf("airbrakes off\n");
+		velocity += force / mass * TIME_DELTA; //meters per second
+		printf("height = %f\nvelocity = %f\nbrake = %f\nthrust = %f\ntime = %f\n\n", height, velocity, brake, thrust, time);
+	}
 	for (; velocity > 0; time += TIME_DELTA) {
 		tend = (GOAL_HEIGHT - height) * 2  / velocity; //seconds
 		fneed = velocity * mass / tend; //newtons
@@ -80,19 +96,13 @@ int main(void)
 			printf("airbrakes on\n");
 		}
 		else {
-			if (height >= GOAL_HEIGHT) {
-				force = -drag - brake - weight; //newtons
-				printf("airbrakes on\n");
-			}
-			else if (height < GOAL_HEIGHT) {
-				force = -drag - weight; //newtons
-				printf("airbrakes off\n");
-			}
+			force = -drag - weight; //newtons
+			printf("airbrakes off\n");
 		}
 
 		velocity += force / mass * TIME_DELTA; //meters per second
 
-		printf("height = %f\nvelocity = %f\ntime = %f\n\n", height, velocity, time);
+		printf("height = %f\nvelocity = %f\nbrake = %f\ntime = %f\n\n", height, velocity, brake, time);
 	}
 	return 0;
 }
