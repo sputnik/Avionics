680f87968e881aa41d4bd784a9d00cf5a48bc171
JakeSammons <jsammons@iastate.edu>
2018-02-14 19:34:10 -0600

    Attempted to use multiple fits for different stages of flight to better the precision of airbrake deployment
    The code works but there is some error in the fit used for the less than 2500 meter portion that causes it to deploy airbrakes to late, I will continue to troubleshoot

--- flight.c
+++ flight.c
@@ -46,6 +46,7 @@ int main(void)
 	double fneed; //newtons
 	double y;
 	double x;
+	double z;
 	double t = 0;
 	double a = 0;
 	double b = 0;
@@ -97,11 +98,21 @@ int main(void)
 		drag = drag_from_density_and_velocity(density, velocity); //newtons
 		brake = brake_from_density_and_velocity(density, velocity); //newtons
 		x = height;
+		if (height <= 2500) {
+			//y = (-.00000000000000000000071763*x*x*x*x*x*x*x) + (.0000000000000000078672*x*x*x*x*x*x) - (.000000000000037169*x*x*x*x*x) + (.000000000096678*x*x*x*x) - (.0000001528*x*x*x) + (.0001648*x*x) - (0.2431*x) + (447.04);
+			y = (.0000067304*x*x) - (.13644*x) + 412.04;
+		}
+		else if (height >= 2500) {
+			//y = (-.000000000016705*x*x*x*x*x) + (.0000002291*x*x*x*x) - (.0012558*x*x*x) + (3.4389*x*x) - (4705*x) + (2572900);
+			//y = (-.00000000000000044502*x*x*x*x*x*x*x) + (.0000000000085817*x*x*x*x*x*x) - (.000000070877*x*x*x*x*x) + (.00032499*x*x*x*x) - (.89351*x*x*x) + (1472.9*x*x) - (1348000*x) + (528380000);
+			z = (x - 2852.4) / 167.86;
+			y = (-2.2266*z*z*z*z*z) - (7.2645*z*z*z*z) - (4.8687*z*z*z) + (0.00029288*z*z) - (26.425*z) + 62.608;
+		}
 		//y = (-0.11493 * x) + 395.26;
 		//y = (-.12299 * x) + 407.94;
-		y = (-.00000000000000000005112*x*x*x*x*x*x*x) + (.00000000000000067005*x*x*x*x*x*x) - (.0000000000036784*x*x*x*x*x) + (.000000010943*x*x*x*x) - (.000019025*x*x*x) + (.019321*x*x) - (10.737*x) + (2839);
+		//y = (-.00000000000000000005112*x*x*x*x*x*x*x) + (.00000000000000067005*x*x*x*x*x*x) - (.0000000000036784*x*x*x*x*x) + (.000000010943*x*x*x*x) - (.000019025*x*x*x) + (.019321*x*x) - (10.737*x) + (2839);
 		//y = (-.000000000000029742*x*x*x*x*x) + (.00000000027068*x*x*x*x) - (.00000096078*x*x*x) + (.0016631*x*x) - (1.5184*x) + 856.86;
-		//y = (.000011648*x*x) - (.15042*x) + 421.39;
+		//y = ((-.00000000000000000005112*x*x*x*x*x*x*x) + (.00000000000000067005*x*x*x*x*x*x) - (.0000000000036784*x*x*x*x*x) + (.000000010943*x*x*x*x) - (.000019025*x*x*x) + (.019321*x*x) - (10.737*x) + (2839) + (-.000000000000029742*x*x*x*x*x) + (.00000000027068*x*x*x*x) - (.00000096078*x*x*x) + (.0016631*x*x) - (1.5184*x) + 856.86) / 2;
 		printf("velocity = %f\n", velocity);
 		printf("y = %f\n", y);
 		if (velocity >= y) {
@@ -113,7 +124,7 @@ int main(void)
 			//printf("airbrakes closing");
 		}
 		if (a == 1) {
-			if (t == 2) {
+			if (t >= 2) {
 				force = -drag - brake - weight; //newtons
 				printf("airbrakes open\n");
 			}
@@ -126,16 +137,17 @@ int main(void)
 			}
 		}
 		else if (a == 0) {
-			if (t == 0) {
+			if (t <= 0) {
 				force = -drag - weight;
 				printf("airbrakes closed\n");
 			}
-			else 
-			force = -drag - brake - weight; //newtons
-			force = force * t / 2;
-			percentopen = t * 50;
-			printf("airbrakes closing\nairbrakes %f percent open\n", percentopen);
-			t = t - TIME_DELTA;
+			else if (t >0) {
+				force = -drag - brake - weight; //newtons
+				force = force * t / 2;
+				percentopen = t * 50;
+				printf("airbrakes closing\nairbrakes %f percent open\n", percentopen);
+				t = t - TIME_DELTA;
+			}
 		}
 
 		velocity += force / mass * TIME_DELTA; //meters per second
