179475483711931ed7264e7e62b2f02204624115
ljzasada <ljzasada@iastate.edu>
2018-03-01 11:08:39 -0600

    Worked on Accelerometer
    Also Deleted RTC unneccessarily.

--- skeleton1.c
+++ skeleton1.c
@@ -3,39 +3,41 @@
 
 //first: ignition
 	// until rocket achieves liftoff(?)
-		//gathered information:?
-		//calculated information:?
+		//gathered information:BNO055
+		//calculated information:Calculate and look for initial launch
 		
 //second: thrusting period
 	// until rocket engine is no longer thrusting(by time or by accelerometer)
-		//gathered information: acceleration(accelerometer), time(?), pressure(barometer)
-		//calculated information: velocity(acceleration), altitude(velocity), altitude(pressure)
+		//gathered information: acceleration(accelerometer), time(RTC), pressure(barometer)
+		//calculated information: velocity(acceleration), altitude(velocity), altitude(pressure), Average altitude(velocity) and altitude(barometer)
 
 // third: deceleration period
 	// until a time at which the rocket could brake to a dicks length of goal, even if the rocket is operating at the highest simulated flight(altitude) (get from mechanical group)
-		//gathered information: acceleration(accelerometer), pressure(barometer), time(?)
-		//calculated information: velocity(acceleration), altitude(velocity), altitude(pressure)
+		//gathered information: acceleration(accelerometer), pressure(barometer), time(RTC)
+		//calculated information: velocity(acceleration), altitude(velocity), altitude(pressure), Average altitude(velocity) and altitude(barometer), 
+		//SHOULD BE USING LINE FROM MATLAB TO FIND WHEN TO BRAKE
 
 // fourth: braking period
 	// until a time at which the rocket is at apogee(accelerometer)
-		//gathered information: altitude(accelerometer), pressure(barometer), time(?)
+		//gathered information: altitude(accelerometer), pressure(barometer), time(RTC)
 		//calculated information: velocity(acceleration), altitude(velocity), altitude(pressure), altitude(avg), velocity(AvgAltitude), density(AvgAltitude), DistanceToGoal(AvgAltitude), StoppingDistanceWithoutBrakes(AvgVelocity)
+		//MAKE SURE WE STILL NEED TO BRAKE
 
 // fifth: drogue deployment
 	// until rocket reaches main shute deployment altitude(?chute altimeter?)
-		//gathered information: altitude(barometer?), location(GPS), time(?)
-		//calculated information: ?
+		//gathered information: altitude(barometer?), location(GPS), time(RTC)
+		//calculated information: sending GPS via RTC
 
 // sixth: main deployment
 	// until touchdown(accelerometer)
-		//gathered information: altitude(barometer?), location(GPS), time(?)
-		//calculated information: ?
+		//gathered information: altitude(barometer?), location(GPS), time(RTC)
+		//calculated information: sending GPS via RTC
 
 //Necesary sensors
-	//barometer
-	//accelerometer
-	//time thingy
-	//GPS
+	//barometer(MCP9808)
+	//accelerometer(BNO055)
+	//RTC(DS3231)
+	//GPS(Ultimate GPS)
 
 //Necesary calculations
 	//velocity from acceleration 
