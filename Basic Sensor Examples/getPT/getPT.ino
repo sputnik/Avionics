20e470984ffd55e3883986fbcdaef5b23c3f7db0
jensenm7 <8675307+jensenm7@users.noreply.github.com>
2018-03-08 10:51:25 -0600

    Added altitude from pressure math
    I added a function to calculate the altitude from the pressure and temperature

--- Basic Sensor Examples/getPT/getPT.ino
+++ Basic Sensor Examples/getPT/getPT.ino
@@ -3,6 +3,18 @@
 
 Adafruit_MPL115A2 mpl115a2;
 
+float calc_altitude(float pressure, float temp) 
+{
+  float Po = 1013.25; //Sea Level Pressure (hPa)
+  float alt = 0.0;
+
+  pressure = pressure / 10.0; //Convert to hPa
+
+  alt = ((pow(Po/pressure,1/5.257)-1)*(temp+273.15))/(0.0065);
+
+  return alt;
+}
+
 void setup(void) 
 {
   Serial.begin(9600);
@@ -14,7 +26,7 @@ void setup(void)
 
 void loop(void) 
 {
-  float pressureKPA = 0, temperatureC = 0;    
+  float pressureKPA = 0, temperatureC = 0, altitude = 0.0;  
 
   mpl115a2.getPT(&pressureKPA,&temperatureC);
   Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.print(" kPa  ");
@@ -25,6 +37,10 @@ void loop(void)
 
   temperatureC = mpl115a2.getTemperature();  
   Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
+
+  altitude = calc_altitude(pressureKPA, temperatureC);
+  Serial.print("Altitude:"); Serial.print(altitude, 1); Serial.println(" meters");
   
   delay(1000);
 }
+
