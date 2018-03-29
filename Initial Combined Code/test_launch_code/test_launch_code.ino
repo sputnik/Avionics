1297b0d0805f88456bf1d9bd9b2e6314023a47bc
jensenm7 <8675307+jensenm7@users.noreply.github.com>
2018-03-29 10:13:09 -0500

    Added RTC, BNO & ADXL
    See title

--- Initial Combined Code/test_launch_code/test_launch_code.ino
+++ Initial Combined Code/test_launch_code/test_launch_code.ino
@@ -3,50 +3,70 @@
 #include <Adafruit_BNO055.h>      //AMU 9-Axis orientation sensor & accelerometer
 #include <utility/imumaths.h>
 #include <Adafruit_MPL115A2.h>
+#include <Adafruit_GPS.h>
+#include <SD.h>
+#include "RTClib.h"
+ 
+File myFile;
 
+RTC_DS3231 rtc;
 Adafruit_MPL115A2 mpl115a2;
 
-float time_delta = .50;
-float height = 0;
-float velocity = 0;
-float yAccel = 0;
-float lheight = 0;
-float lvelocity = 0;
-float verticalAccel = 0;
+  float time_delta = .50;
+  float height = 0;
+  float velocity = 0;
+  float yAccel = 0;
+  float lheight = 0;
+  float lvelocity = 0;
+  float verticalAccel = 0;
+  float ADXL_accel[3];
+  float BNO_accel[3];
+  float pressure = 0;
+  
+  char SD_data[400];
+  char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+  
+  bool launchvalue = false;
 
-bool launchvalue = false;
-
-#define BNO055_SAMPLERATE_DELAY_MS (time_delta * 1000)
+  #define BNO055_SAMPLERATE_DELAY_MS (time_delta * 1000)
+  #define GPSSerial Serial1
+  #define GPSECHO true
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
+Adafruit_GPS GPS(&GPSSerial);
 
-float calc_altitude(float pressure, float temp) 
-{
-  float Po = 1013.25; //Sea Level Pressure (hPa)
-  float alt = 0.0;
-
-  pressure = pressure / 10.0; //Convert to hPa
-
-  alt = ((pow(Po/pressure,1/5.257)-1)*(temp+273.15))/(0.0065);
-
-  return alt;
-}
+void get_ADXL_data(float accel_vals[]);
+float get_BNO_change_height();
+float calc_altitude(float pressure, float temp);
 
 void setup(void) 
 {
+  
   Serial.begin(9600);
-  Serial.println("Hello!");
+  Serial.print("Initializing systems...");
   
-  float acceleration[3];
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
+   pinMode(10, OUTPUT);
+ 
+  if (!SD.begin(10)) {
+    Serial.println("SD card initialization failed! Check connections and/or insert a valid microSD card");
+    return;
+  }
+  
+  Serial.println("SD Card initialization successful.");
+    
   mpl115a2.begin();
-  
+
   if (!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
   }
   else {
-    Serial.println("BNO Connected!");
+    Serial.println("BNO initialization successful.");
     if (bno.isFullyCalibrated()) {
       Serial.println("BNO Fully Calibrated!");
     }
@@ -57,9 +77,69 @@ void setup(void)
 
   bno.setExtCrystalUse(true);
   mpl115a2.begin();
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  else {
+    Serial.println("RTC initialization successful.");
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+  
 }
 
-void get__BNO_data() {
+
+
+void loop() {
+  get_ADXL_data(ADXL_accel);
+
+  sprintf(SD_data, "The height is: %lf\n
+                    The velocity is: %lf\n
+                    The acceleration is: %lf\n
+                    The pressure is: %lf\n
+                    The time is: %d:%d:%d\n
+                    The date is: %d/%d/%d\n", height, velocity, verticalAccel, );
+
+}
+
+void write_to_SD(char SD_info[]) {
+  
+  Serial.print("Writing data to SD...");
+  rocket_data = SD.open("rocket_data.txt",FILE_WRITE);
+  rocket_data.println(SD_info);
+  rocket_data.close();
+  Serial.println("DONE!");
+  
+}
+
+float get_BNO_change_height() {
+
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    imu::Vector<3> linear = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+    imu::Vector<3> gravity = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+ 
+    verticalAccel = ((linear.x() * gravity.x()) + (linear.y() * gravity.y()) + (linear.z() * gravity.z())) / (-9.81);
+    if (verticalAccel >= .5) {
+      launchvalue = true ;
+    }
+    if (verticalAccel <= -.5) {
+      launchvalue = true ;
+    }
+    if (launchvalue = false); {
+      verticalAccel = 0;
+    }
+    lheight = lheight + (lvelocity * time_delta) + (.5 * verticalAccel * time_delta * time_delta);
+    lvelocity = lvelocity + (verticalAccel * time_delta);
 
 }
 
@@ -89,7 +169,14 @@ void get_ADXL_data(accel_vals[]) {
 
 }
 
-void loop() {
-  get_ADXL_data(acceleration);
+float calc_altitude(float pressure, float temp) 
+{
+  float Po = 1013.25; //Sea Level Pressure (hPa)
+  float alt = 0.0;
 
+  pressure = pressure / 10.0; //Convert to hPa
+
+  alt = ((pow(Po/pressure,1/5.257)-1)*(temp+273.15))/(0.0065);
+
+  return alt;
 }
