9eae639763e72fd20db4fc9387765573ba704ea3
JakeSammons <jsammons@iastate.edu>
2018-04-17 00:59:31 -0500

    Removed unneccessary code from airbrake calculation

--- Main Running Code/launch_code/Air_Brake_Calculations.ino
+++ Main Running Code/launch_code/Air_Brake_Calculations.ino
@@ -1,112 +1,44 @@
-  
-  double time; //seconds
-  double alt = START_ALT; //meters
-  double height = 0; //meters
-  double velocity = 0; //meters per second
-  double mass; //kg
-  double density; //kg per cubic meter
-  double drag; //newtons
-  double weight; //newtons
-  double force; //newtons
-  double thrust; //newtons
-  double brake; //newtons
-  double tend; //seconds
-  double fneed; //newtons
   double y;
   double x;
-  double z;
   double t = 0;
   double a = 0;
-  double b = 0;
   double percentopen;
 
 
-void check_airbrakes() {
-  
-  thrust = START_THRUST; //newtons
-  weight = WEIGHT_I; //newtons
-  for (time = 0; time < 4.8; time += TIME_DELTA) {
-    if (time <= 4.0) {
-      thrust = 3000;
-    }
-    else if (time > 4.0) {
-      thrust -= (3750) * TIME_DELTA;
-    } //thrust equation
-
-    alt += velocity * TIME_DELTA; //meters
-    height = alt - START_ALT; //meters
-    density = density_from_altitude(alt); //kg per cubic meter
     drag = drag_from_density_and_velocity(density, velocity); //newtons
-    weight -= FUEL_BURN_RATE * TIME_DELTA; //newtons
-    force = thrust - drag - weight; //newtons
-    mass = weight / G; //kg
-    velocity += force / mass * TIME_DELTA; //meters per second
-    fprintf(logfile, "height = %f\nvelocity = %f\nthrust = %f\ndrag = %f\ntime = %f\n\n", height, velocity, thrust, drag, time);
+void check_airbrakes() {  
+  x = height;
+  y = (-.00000000000001899*x*x*x*x*x) + (.00000000015956*x*x*x*x) - (.00000051519*x*x*x) + (.00079063*x*x) - (.65635 * x) + (480.28);
+  if (velocity >= y) {
+    a = 1;
+    //printf("airbrakes opening\n");
   }
-
-  weight = WEIGHT_F; //newtons
-  mass = weight / G; //kg
-  for (; time < 4.8; time += TIME_DELTA) {
-    alt += velocity * TIME_DELTA; //meters
-    height = alt - START_ALT; //meters
-    density = density_from_altitude(alt); //kg per cubic meter
-    drag = drag_from_density_and_velocity(density, velocity); //newtons
-    brake = brake_from_density_and_velocity(density, velocity); //newtons
-    force = -drag - weight; //newtons
-    fprintf(logfile, "airbrakes off\n");
-    velocity += force / mass * TIME_DELTA; //meters per second
-    fprintf(logfile, "height = %f\nvelocity = %f\nbrake = %f\nthrust = %f\ntime = %f\n\n", height, velocity, brake, thrust, time);
+  else if (velocity < y) {
+    a = 0;
+    //printf("airbrakes closing");
   }
-  for (; velocity > 0; time += TIME_DELTA) {
-    alt += velocity * TIME_DELTA; //meters
-    height = alt - START_ALT; //meters
-    density = density_from_altitude(alt); //kg per cubic meter
-    drag = drag_from_density_and_velocity(density, velocity); //newtons
-    brake = brake_from_density_and_velocity(density, velocity); //newtons
-    x = height;
-    if (height <= 2500) {
-      y = (-.000000000000019001*x*x*x*x*x) + (.00000000015977*x*x*x*x) - (.00000051643*x*x*x) + (.0007944*x*x) - (.66328 * x) + (486.62);
-      //y = (-.00000000000001899*x*x*x*x*x) + (.00000000015956*x*x*x*x) - (.00000051519*x*x*x) + (.00079063*x*x) - (.65635 * x) + (480.28);
+  if (a == 1) {
+    if (t >= ACTUATION_TIME) {
+      Open Airbrakes;
+      //they are all of the way open
+      percentopen = 100;
     }
-    else if (height >= 2500) {
-      y = (-.000000000000019001*x*x*x*x*x) + (.00000000015977*x*x*x*x) - (.00000051643*x*x*x) + (.0007944*x*x) - (.66328 * x) + (486.62);
-      //y = (-.00000000000001899*x*x*x*x*x) + (.00000000015956*x*x*x*x) - (.00000051519*x*x*x) + (.00079063*x*x) - (.65635 * x) + (480.28);
+    else if (t < ACTUATION_TIME) {
+      Open Airbrakes;
+      percentopen = t * 45.4545454545;
+      t = t + TIME_DELTA;
     }
-    fprintf(logfile, "velocity = %f\n", velocity);
-    fprintf(logfile, "y = %f\n", y);
-    if (velocity >= y) {
-      a = 1;
-      //printf("airbrakes opening\n");
+  }
+  else if (a == 0) {
+    if (t <= 0) {
+      Close Airbrakes;
+      //they are all of the way closed
+      percentopen = 0;
     }
-    else if (velocity < y) {
-      a = 0;
-      //printf("airbrakes closing");
+    else if (t >0) {
+      Close Airbrakes
+      percentopen = t * 33.33333;
+      t = t - TIME_DELTA;
     }
-    if (a == 1) {
-      if (t >= ACTUATION_TIME) {
-        force = -drag - brake - weight; //newtons
-        fprintf(logfile, "airbrakes open\n");
-      }
-      else if (t < ACTUATION_TIME) {
-        brake = brake * t / ACTUATION_TIME;
-        force = -drag - brake - weight; //newtons
-        percentopen = t * 33.33333;
-        t = t + TIME_DELTA;
-      }
-    }
-    else if (a == 0) {
-      if (t <= 0) {
-        force = -drag - weight;
-      }
-      else if (t >0) {
-        brake = brake * t / ACTUATION_TIME;
-        force = -drag - brake - weight; //newtons
-        percentopen = t * 33.33333;
-        t = t - TIME_DELTA;
-      }
-    }
-    //force = -drag - weight;
-    velocity += force / mass * TIME_DELTA; //meters per second
-
   }
 }
