efecab5ae11ec90b8d92bb5808bb024134b2fbb7
ljzasada <ljzasada@iastate.edu>
2018-04-19 10:14:31 -0500

    Variable Fixes

--- Main Running Code/launch_code/VariableDefinitions.h
+++ Main Running Code/launch_code/VariableDefinitions.h
@@ -2,6 +2,15 @@
 #define VariableDefinitions.h
 
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>      //AMU 9-Axis orientation sensor & accelerometer
+#include <utility/imumaths.h>
+#include <Adafruit_MPL115A2.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <SD.h>
+
 //Defines for the Air Brake Calculations
 #define G               9.81          // Gravity...Duh (meters per second squared)
 #define SEA_DENSITY     1.225         // Air Density at Sea Level (kg per cubic meter)
@@ -13,6 +22,7 @@
 #define START_THRUST    2500          // Starting Thrust (newtons)
 #define FUEL_BURN_RATE  14.22991494   // Weight in fuel Burned Per Unit Time (newtons per second)
 #define ACTUATION_TIME  2.2             // Time to fully Extend the Airbrakes (seconds)
+#define AIRBRAKE_PIN 4
 
 int START_ALT = 0;
 int current_status = 0;
@@ -23,12 +33,26 @@ double PercentOpen = 0.0;
 double ActuationTime = 0.0;
 bool AirbrakeStatus = false;
 
+//RTC
+RTC_DS3231 rtc;
 
 //Defines for the GPS
 //#define RXPin   6     // Software Serial RX pin
 //#define TXPin   5     // Software Serial TX pin
 //#define GPSBaud 9600  // Baud Rate
 
+//function declarations
+void check_airbrakes();
+void get_Alt_BNO();
+float get_Alt_Pressure();
+void get_Accel_ADXL();
+void get_Avg_Alt();
+void write_to_SD(char SD_info[]);
+int get_current_status();
+void while_on_pad();
+void while_launching();
+void while_still_rising();
+void while_descending();
 
 double AvgHeight = 0;
 double AvgVelocity;
@@ -45,13 +69,13 @@ float IterationStartTime;
 float IterationEndTime;
 float ComputationTime;
 float WaitTime;
+float pressure;
 
 
 //SD Card
 File rocket_data;
-#define SDCS_pin  5
+#define SDCS_pin 5
 char SD_data[400];
-extern SDCS_pin;
 
 // Variables/Defines for doing the Altitude Calculations (Pressure Sensor)
 #define SEA_LEVEL_PRESSURE              1013.25 //Sea Level Pressure (hPa)
