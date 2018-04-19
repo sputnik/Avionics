4b8ce21e9fc13f2845cb4cd6d338d89a08b93d33
jensenm7 <8675307+jensenm7@users.noreply.github.com>
2018-04-19 10:36:45 -0500

    Added RTC Functionality
    Added RTC file
    Added rtc defines
    added rtc to init

---
+++ Main Running Code/launch_code/RTC_functions.ino
@@ -0,0 +1,11 @@
+void get_Time() {
+
+  DateTime now = rtc.now();
+  rtc_Second = now.second();
+  rtc_Minute = now.minute();
+  rtc_Hour = now.hour();
+  rtc_Day = now.day();
+  rtc_Month = now.month();
+  rtc_Year = now.year();
+
+}
