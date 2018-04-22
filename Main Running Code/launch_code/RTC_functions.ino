87cac1c7e6bf4e98f3929a78b6e7d1c4a476c694
jensenm7 <8675307+jensenm7@users.noreply.github.com>
2018-04-22 14:12:29 -0500

    Comment
    comment

--- Main Running Code/launch_code/RTC_functions.ino
+++ Main Running Code/launch_code/RTC_functions.ino
@@ -1,11 +1,12 @@
-void get_Time() {
-
-  DateTime now = rtc.now();
-  rtc_Second = now.second();
-  rtc_Minute = now.minute();
-  rtc_Hour = now.hour();
-  rtc_Day = now.day();
-  rtc_Month = now.month();
-  rtc_Year = now.year();
-
-}
+//
+//void get_Time() {
+//
+//  DateTime now = rtc.now();
+//  rtc_Second = now.second();
+//  rtc_Minute = now.minute();
+//  rtc_Hour = now.hour();
+//  rtc_Day = now.day();
+//  rtc_Month = now.month();
+//  rtc_Year = now.year();
+//
+//}
