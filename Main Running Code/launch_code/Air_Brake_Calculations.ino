b4b5e61b19f95b20a2b519b855c7d95c31da32a1
JakeSammons <jsammons@iastate.edu>
2018-05-02 20:42:07 -0500

    made updates for north ridge

--- Main Running Code/launch_code/Air_Brake_Calculations.ino
+++ Main Running Code/launch_code/Air_Brake_Calculations.ino
@@ -3,8 +3,9 @@
 void check_airbrakes() {  
     //#error "Need AvgHeight and AvgVelocity passed in. It shouldnt need them as inputs because they are global variables"
   double x = AvgHeight;
-  //VelocityPlot = (-.00000000000001899*x*x*x*x*x) + (.00000000015956*x*x*x*x) - (.00000051519*x*x*x) + (.00079063*x*x) - (.65635 * x) + (480.28);
+  //VelocityPlot = (-.000000000000019026*x*x*x*x*x) + (.00000000015991*x*x*x*x) - (.00000051648*x*x*x) + (.00079253*x*x) - (.65636 * x) + (478.29);
   VelocityPlot = 40 - 8*x; // Modified plot for testing in elevator
+  
   if (AvgVelocity >= VelocityPlot) {
     AirbrakeStatus = true;
     digitalWrite(LA_DIR, HIGH);
@@ -20,31 +21,31 @@ void check_airbrakes() {
   Serial.println(AirbrakeStatus);
   AirbrakeValue = analogRead(LA_FDBK);
   if (AirbrakeStatus == true) {
-    if (CurrentActuationPeriod >= ACTUATION_TIME) {
+    if (LAfdbk >= 500) {
       digitalWrite(LA_DIR, HIGH);
       LAdir = 1;
       digitalWrite(LA_PWM, LOW);
       LApwm = 0;
-      PercentOpen = 100;
+      //PercentOpen = 100;
     }
-    else if (CurrentActuationPeriod < ACTUATION_TIME) {
+    else if (LAfdbk < 500) {
       digitalWrite(LA_DIR, HIGH);
       LAdir = 1;
       digitalWrite(LA_PWM, HIGH);
       LApwm = 1;
-      PercentOpen = CurrentActuationPeriod * 45.4545454545;
-      CurrentActuationPeriod = CurrentActuationPeriod + TIME_DELTA;
+      //PercentOpen = CurrentActuationPeriod * 45.4545454545;
+      //CurrentActuationPeriod = CurrentActuationPeriod + TIME_DELTA;
     }
   }
   else if (AirbrakeStatus == false) {
-    if (CurrentActuationPeriod <= 0) {
+    if (LAfdbk <= 208) {
       digitalWrite(LA_DIR, LOW);
       LAdir = 0;
       digitalWrite(LA_PWM, LOW);
       LApwm = 0;
       PercentOpen = 0;
     }
-    else if (CurrentActuationPeriod >0) {
+    else if (LAfdbk > 208) {
       digitalWrite(LA_DIR, LOW);
       LAdir = 0;
       digitalWrite(LA_PWM, HIGH);
