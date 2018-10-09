edd6e18550c47156fc18ce13ca79b519cf579307
JakeSammons <jsammons@iastate.edu>
2018-10-09 18:55:57 -0500

    added comments about how data is printed to the sd card
    added a comment regarding thhe meaning of SD_data

--- Telemetry/write_to_SD/write_to_SD.ino
+++ Telemetry/write_to_SD/write_to_SD.ino
@@ -4,15 +4,16 @@ void write_to_SD() {
   // if the file opened okay, write to it:
   if (myFile) {
     
-    Serial.print("Writing to rocket.txt...");
+    Serial.print("Writing to Telemetry.txt...");
     myFile.println(SD_data);
+    // SD_data is a character array that is written to when the values that are to be saved on the sd card are printed with sprintf. The size of the character array limits how many characters can be printed
        
     // close the file:
     myFile.close();
-    Serial.println("done.");
+    Serial.println("Telemetry.txt successfully closed.");
   } 
   else {
     // if the file didn't open, print an error:
-    Serial.println("error opening rocket.txt");
+    Serial.println("error opening Telemetry.txt");
   }
 
