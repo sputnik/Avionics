62be62f695e35e8fc8e464d93ecb9ab4e8debd25
garrett <grmiller@iastate.edu>
2018-11-04 18:41:15 -0600

    Updated spacing/style of Data.h

--- Airbrakes/src/Data.h
+++ Airbrakes/src/Data.h
@@ -1,5 +1,5 @@
 #ifndef DATA_H
-# define DATA_H
+#define DATA_H
 
 struct Data {
   double accX;
@@ -19,16 +19,15 @@ struct Data {
    * Constructor.
    *  Initializes data to default values
    */
-  Data()
-  {
+  Data() {
     accX = 0.0;
     accY = 0.0;
     accZ = 0.0;
-	accV = 0.0;
+    accV = 0.0;
     velX = 0.0;
     velY = 0.0;
     velZ = 0.0;
-	velV = 0.0;
+    velV = 0.0;
     alt = 0.0;
     pressure = 0.0;
     airbrakes = 0.0;
@@ -40,48 +39,45 @@ struct Data {
    * @param other
    *    The other Data object to copy into this
    */
-  Data(Data& other)
-  {
+  Data(Data &other) {
     accX = other.accX;
     accY = other.accY;
     accZ = other.accZ;
-	accV = other.accV;
+    accV = other.accV;
     velX = other.velX;
     velY = other.velY;
     velZ = other.velZ;
-	velV = other.velV;
+    velV = other.velV;
     alt = other.alt;
     pressure = other.pressure;
     airbrakes = other.airbrakes;
     t = other.t;
   }
 
-  void clone(Data& other)
-  {
+  void clone(Data &other) {
     accX = other.accX;
     accY = other.accY;
     accZ = other.accZ;
-	accV = other.accV;
+    accV = other.accV;
     velX = other.velX;
     velY = other.velY;
     velZ = other.velZ;
-	velV = other.velV;
+    velV = other.velV;
     alt = other.alt;
     pressure = other.pressure;
     airbrakes = other.airbrakes;
     t = other.t;
   }
 
-  void clone(Data* other)
-  {
+  void clone(Data *other) {
     accX = other->accX;
     accY = other->accY;
     accZ = other->accZ;
-	accV = other->accV;
+    accV = other->accV;
     velX = other->velX;
     velY = other->velY;
     velZ = other->velZ;
-	velV = other->velV;
+    velV = other->velV;
     alt = other->alt;
     pressure = other->pressure;
     airbrakes = other->airbrakes;
