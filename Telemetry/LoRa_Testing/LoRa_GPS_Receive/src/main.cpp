c1a4135a59295388ba08b75bffe19bd6dd833cf1
Garrett <garrettstillwater@gmail.com>
2018-12-02 11:49:10 -0600

    Modifications to LoRa receive to write to SD card

--- Telemetry/LoRa_Testing/LoRa_GPS_Receive/src/main.cpp
+++ Telemetry/LoRa_Testing/LoRa_GPS_Receive/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <RH_RF95.h>
+#include <SD.h>
 #include <SPI.h>
 #include <TinyGPS++.h>
+
 #define REC_LAT 45.0560
 #define REC_LON -92.8088
+#define REC_ALT 915 // (m);
 
 #define RFM95_CS 9
 #define RFM95_RST 10
@@ -12,6 +15,10 @@
 #define RF95_FREQ 915.0
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 TinyGPSPlus gps;
+
+// Set the pins used
+#define cardSelect 4
+File logfile;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -41,7 +48,26 @@ void setup() {
   }
   Serial.print("Set Freq to: ");
   Serial.println(RF95_FREQ);
-  // put your setup code here, to run once:
+  // init SD card
+  // see if the card is present and can be initialized:
+  if (!SD.begin(cardSelect)) {
+    Serial.println("Card init. failed!");
+  }
+  char filename[15];
+  strcpy(filename, "ANALOG00.TXT");
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = '0' + i / 10;
+    filename[7] = '0' + i % 10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (!SD.exists(filename)) {
+      break;
+    }
+  }
+  logfile = SD.open(filename, FILE_WRITE);
+  if (!logfile) {
+    Serial.print("Couldnt create ");
+    Serial.println(filename);
+  }
 }
 
 void loop() {
@@ -69,6 +95,14 @@ void loop() {
       Serial.print("dist = ");
       Serial.print(distanceToGps);
       Serial.println("(m)");
+      logfile.print("Rec: lat");
+      logfile.print(lat);
+      logfile.print(", lon = ");
+      logfile.print(lon);
+      logfile.print(", dist = ");
+      logfile.print(distanceToGps);
+      logfile.print(" (m)");
+      logfile.flush();
     } else {
       Serial.println("recv failed");
     }
