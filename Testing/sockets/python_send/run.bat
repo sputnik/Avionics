e7ee32f2ba1b1242dc30b539586bbf33f6bd82a6
Garrett <garrettstillwater@gmail.com>
2018-12-21 13:23:38 -0600

    Updates to run.bat so the directory the code is in doesn't matter.

--- Testing/sockets/python_send/run.bat
+++ Testing/sockets/python_send/run.bat
@@ -1,8 +1,9 @@
 set mypath=%cd%
+set direc=%mypath:~0,2%
 set mypath=%mypath:~3%
 set mypath=%mypath:\=/%
 taskkill /im pythonw.exe /t /f
-C:\cygwin64\bin\bash --login -c "cd /cygdrive/c/%mypath%; g++ client.cpp"
+C:\cygwin64\bin\bash --login -c "cd %direc%; cd %mypath%; g++ client.cpp"
 start C:\Python37\pythonw.exe C:\Python37\Lib\idlelib\idle.pyw -r server.py
-C:\cygwin64\bin\bash --login -c "cd /cygdrive/c/%mypath%; ./a.exe"
+C:\cygwin64\bin\bash --login -c "cd %direc%; cd %mypath%; ./a.exe"
 pause
